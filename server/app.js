23f930f6f1de3dbb3f5ee8403ed23170b14f8bbb
Linden Chiu <hchiu87@gmail.com>
2020-07-10 20:33:49 -0700

    clean up

--- server/app.js
+++ server/app.js
@@ -1,18 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const foodRouter = require('./routes/food');
+const routes = require('./routes/index');
 
-var MongoClient = require('mongodb').MongoClient
+
+const MongoClient = require('mongodb').MongoClient
 MongoClient.connect('mongodb://localhost:27017')
 
+const app = express();
 
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,9 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/food', foodRouter);
+app.use('/', routes);
+app.use('/user', routes);
+app.use('/food', routes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -44,6 +43,11 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port 8000!')
+})
 
 
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const db = require('../seedDB');
+const cheerio = require('cheerio');
+const request = require('request');
+const wordOfDay = [];
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send('hello');
+  res.send('Hello World!');
+});
+
+router.get('/food', function (req, res, next) {
+  res.send(
+    JSON.stringify(db.foodInfo)
+  );
+});
+
+router.get('/:id', function (req, res) {
+  console.log(req.params)
+  res.send( JSON.stringify(db.foodInfo[req.params.id]))
+});
+
+router.get('/user', function(req, res, next) {
+  res.send('Respond with a resource');
 });
 
 module.exports = router;
