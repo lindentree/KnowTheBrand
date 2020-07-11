d79332b64f5957850d871e5224066c66abfc33b8
chiexplores <contact@chihuynh.me>
2020-07-10 22:14:03 -0700

    deleted uncessary variables and added route for name parameter

--- server/controller/brands.js
+++ server/controller/brands.js
@@ -2,12 +2,13 @@ const Brand = require('../models/brand');
 
 module.exports = {
   find,
-  save,
+  show,
 }
 
 
 function find(req, res) {
   let name = req.params.name
+  console.log(req.params)
   Brand.find({ name: name })
     .then((err, brand) => {
       if (err) res.send(err);
@@ -18,7 +19,7 @@ function find(req, res) {
 
 //TODO check JSON.parse(JSON.stringify(brand)) 
 
-function save(req, res) {
+function show(req, res){
+  //grab all from db
 
 }
-
