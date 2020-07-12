e66f54b1118b44d497998bdb210c7694fb7ea675
chiexplores <contact@chihuynh.me>
2020-07-12 10:08:05 -0700

    delete commented code

--- server/controller/brands.js
+++ server/controller/brands.js
@@ -10,12 +10,8 @@ async function find(req, res) {
 
   try {
     let name = req.params.name
-   
-
     const result = await Brand.find({ name: name })
     console.log("result: ", result)
-
-
     res.send(result);
 
   } catch(err) {
@@ -24,23 +20,6 @@ async function find(req, res) {
       })
 
   }
- 
-  //if (err) res.send(err);
-    // .then((err, brand) => {
-
-    //   // res.send(JSON.stringify(brand));
-
-    //   // if (err) res.send(err);
-    //   if (err) {
-    //     return err;
-    //   } else {
-    //     return brand;
-    //   }
-
-
-
-      
-    // });
 }
 
 function show(req, res) {
