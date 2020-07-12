9b6886ec0d1132e099250864b08ce8f2e75b9d32
Linden Chiu <hchiu87@gmail.com>
2020-07-12 15:05:32 -0700

    add mission statement

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ function App() {
        <h1> Know Your Brand </h1>
       
       </header>
+          <h3> 
+            Mission: We want users to understand where you are putting your money. Search a brand and learn more about the brand. You are, what you buy. Normally, we just know the brand name, but do we ever question who the brand is and who is behind it?
+
+          </h3>
           <TextInput />
           <ImageUpload />
       
