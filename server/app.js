62f7446a70992f6c350c777453c416b50c504c7e
chiexplores <contact@chihuynh.me>
2020-07-12 12:57:30 -0700

    dotenv module

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const routes = require('./routes/index');
+require('dotenv').config();
 
 //connect to database
 require('./config/database');
