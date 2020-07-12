ed8c0740dbb755efa8c88355c7cd7ddbeb8f5e20
chiexplores <contact@chihuynh.me>
2020-07-11 21:57:51 -0700

    fix faux seed data

--- server/seedDB.js
+++ server/seedDB.js
@@ -3,14 +3,12 @@
 var foodInfo = exports.foodInfo = [];
 
 foodInfo.push({
-  id: 0,
-  name: "Blue Diamond",
+  name: "Blue Diamond Growers",
   headquarters: "Sacramento, CA",
   description: "Blue Diamond Growers is a California agricultural cooperative and marketing organization that specializes in almonds. Founded in 1910 as the California Almond Grower's Exchange, the organization claims to be the world's largest tree nut processing and marketing company. It serves 3,500 almond growers, and helps make the almond crop (valued at over $1 billion) California's largest food export. -Wikipedia"
 });
 
 foodInfo.push({
-  id: 1,
   name: "Sun-Maid",
   headquarters: "Fresno, CA",
   description: "Sun-Maid Growers of California is a privately owned American cooperative of raisin growers headquartered in Kingsburg, California. Sun-Maid is the largest raisin and dried fruit processor in the world. As a cooperative, Sun-Maid is made up of approximately 850 family farmers who grow raisin grapes within a 100-mile (160-kilometer) radius of the processing plant. Sun-Maid also sources dried fruit beyond this geographical area. In 2012, Sun-Maid celebrated its 100th anniversary as a grower cooperative. -Wikipedia"
