0b405861bb3757b7c98224b5b8d3bdd7f7a4b18b
chiexplores <contact@chihuynh.me>
2020-07-11 21:47:02 -0700

    stubbed show function

--- server/controller/brands.js
+++ server/controller/brands.js
@@ -12,14 +12,12 @@ function find(req, res) {
   Brand.find({ name: name })
     .then((err, brand) => {
       if (err) res.send(err);
-
+      console.log(brand)
       res.send(JSON.stringify(brand))
     });
 }
 
-//TODO check JSON.parse(JSON.stringify(brand)) 
-
-function show(req, res){
+function show(req, res) {
   //grab all from db
 
 }
