c539d8255671d808b1179893dc701c7f63807ba0
chiexplores <contact@chihuynh.me>
2020-07-11 21:58:05 -0700

    changes:     - route mounted as api/food

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,26 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const brandCtrl = require('../controller/brands');
+const path = require('path')
 
 const db = require('../seedDB');
 const cheerio = require('cheerio');
 const request = require('request');
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-router.get('/food', function (req, res, next) {
+router.get('/', function (req, res, next) {
   res.send(
     db.foodInfo
   );
 });
-
-// router.get('/:id', function (req, res) {
-//   console.log(req.params);
-//   res.send(JSON.stringify(db.foodInfo[req.params.id]))
-// });
-
 router.get('/:name', brandCtrl.find);
 
 
