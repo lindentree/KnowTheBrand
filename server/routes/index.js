5b4b207d415013c368a500cfe4f39259d1929f9e
chiexplores <contact@chihuynh.me>
2020-07-12 12:58:11 -0700

    stub route to show all brands

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const brandCtrl = require('../controller/brands');
-const path = require('path')
+
 
 const db = require('../seedDB');
 const cheerio = require('cheerio');
 const request = require('request');
 
-router.get('/', function (req, res, next) {
-  res.send(
-    db.foodInfo
-  );
-});
+router.get('/', brandCtrl.show);
 router.get('/:name', brandCtrl.find);
 
 
